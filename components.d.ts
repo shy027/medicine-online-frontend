3c8c42e60e73d4917432d8cf1f627a0795fc26e3
shy <1533103845@qq.com>
2024-12-02 23:49:00 +0800

    feat: 完成登录页面静态页面内容

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Bottom: typeof import('./src/components/index/Bottom.vue')['default']
     BottomList: typeof import('./src/components/home/BottomList.vue')['default']
     Header: typeof import('./src/components/index/Header.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
---
+++ src/components/index/Bottom.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg flex pl-150 pt-20">
+    <div class="text-white">
+      <p class="text-h5"><b>联系我们</b></p>
+      <p class="text">微信公众号:凤凰杏林学堂</p>
+      <p class="text">QQ:364007372</p>
+      <p class="text">Email:18761663770@163.com</p>
+      <p class="text">备案号: 苏ICP备10204884号-5</p>
+    </div>
+    <div class="mx-5 pic pl-30">
+      <img src="https://www.helloimg.com/i/2024/12/02/674d425107a37.jpg" />
+      <p class="text-white mt-3">江苏中医在线小程序</p>
+    </div>
+    <div class="mx-5 pic">
+      <img src="https://www.helloimg.com/i/2024/12/02/674d42749ab6f.png" />
+      <p class="text-white mt-3">凤凰杏林学堂</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.bg{
+  background-color: rgb(53, 52, 52);
+  height: 330px;
+}
+img{
+  width: 150px;
+}
+.text{
+font-size: small;
+margin-top: 20px;
+}
+.pic{
+  text-align:center;
+}
+</style>
